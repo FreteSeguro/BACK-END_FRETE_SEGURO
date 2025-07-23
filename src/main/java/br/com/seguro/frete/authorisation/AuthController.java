47196313337470934e96f6f1bcba5e8e94c851a7
PaulinhoVieira <soeupfv@hotmail.com>
2025-07-23 18:13:04 -0300

    removido importaçoes que nao estavam em uso

--- src/main/java/br/com/seguro/frete/authorisation/AuthController.java
+++ src/main/java/br/com/seguro/frete/authorisation/AuthController.java
@@ -1,18 +1,14 @@
 package br.com.seguro.frete.authorisation;
 
-import java.util.Map;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.seguro.frete.user.User;
 import br.com.seguro.frete.user.UserCreateDTO;
